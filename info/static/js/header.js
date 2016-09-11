3647b43bcbe0d209c989726332c4696ec1ee6e8d
KovaVeikko <veikko.kovanen@gmail.com>
2016-09-11 23:07:52 +0300

    fixed header dynamic text script

--- info/static/js/header.js
+++ info/static/js/header.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       var document_position = $(window).scrollTop();
       var dividers = document.getElementsByClassName('category-divider');
       for (var i = 0; i<dividers.length; i++) {
-        var position = $(dividers[i]).position().top;
+        var position = $(dividers[i]).offset().top;
         if (document_position >= position) {
           $(".navbar-brand").html($(dividers[i]).attr('category'));
         }
