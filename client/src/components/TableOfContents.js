c1deec54b2dcefcd07a128bfc3a984f1c941d2cf
Veikko Kovanen <veikko.kovanen@gmail.com>
2018-01-09 00:13:56 +0200

    style table of contents

--- client/src/components/TableOfContents.js
+++ client/src/components/TableOfContents.js
@@ -5,19 +5,18 @@ class TableOfContents extends Component {
   render() {
     return (
       <div id="table-of-contents">
-        <div>
-          {this.props.content.data.map((category, key) =>  category.messages.length > 0 ? (
-            <div key={key}>
-              <span>{category.title}</span>
-              <ul>
-                {category.messages.map((message, key) => (
-                  <li key={key}>{message.header}</li>
-                ))}
-              </ul>
-            </div>
+        {this.props.content.data.map((category, key) =>  category.messages.length > 0 ? (
+          <ul key={key}>
+            <li className="category-title">{category.title}</li>
+            <ul>
+              {category.messages.map((message, key) => (
+                <li key={key} className="message-title"><a href={`#${message.id}`}>{message.header}</a></li>
+              ))}
+            </ul>
+          </ul>
 
-          ) : null)}
-        </div>
+        ) : null)}
+        <hr/>
       </div>
     )
   }
