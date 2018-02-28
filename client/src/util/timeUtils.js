27a4fd8bf4bfd51fd9dab57b60c46869f9254bc4
Veikko Kovanen <veikko.kovanen@gmail.com>
2018-02-28 18:37:15 +0200

    remove timeUtils

--- client/src/util/timeUtils.js
+++
@@ -1,8 +0,0 @@
-
-/* is the given date lass than three days ago? */
-export const isNew = (dateStr) => {
-  const today = new Date()
-  const date = new Date(dateStr)
-  const diff = Math.round((today-date)/(1000*60*60*24))
-  return diff < 3
-}
