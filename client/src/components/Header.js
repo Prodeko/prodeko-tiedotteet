e87e887805de5a3e49fcc839f2e7f1a823064807
Veikko Kovanen <veikko.kovanen@gmail.com>
2018-02-28 18:08:32 +0200

    add banner + change font to Railway

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div id="header">
-        <span>Prodeko - Tiedotteet</span>
-      </div>
+      <div id="header"/>
     )
   }
 }
